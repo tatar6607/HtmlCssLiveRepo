a90e41a2f45a910bc53e2e32478d595546f43b6f
Ozkan <tatar66@gmail.com>
2021-05-25 16:43:58 +0200

    onmouseover-out func added

--- pages/Extras/js/app.js
+++ pages/Extras/js/app.js
@@ -15,23 +15,30 @@ buttonsValues.forEach(element => {
 })
 
 const checkNum = (selectedNum) => {
-    screen.innerHTML == "0.0"  
+    screen.innerHTML == "0"  
     ? screen.innerHTML = selectedNum 
     : screen.innerHTML += selectedNum;
 
-    if(selectedNum=="AC") {
-        screen.innerHTML = "0.0";
-        console.log(screen.innerHTML);
-    }else {
-    
-    }
+    // screen.innerHTML == "0" && selectedNum == "x" || screen.innerHTML == "0" && selectedNum == "&divide;"
+    // ? screen.innerHTML = "0" 
+    // : screen.innerHTML += selectedNum;    
 
-       
+    if(selectedNum=="AC") {
+        screen.innerHTML = "0";
+        console.log(screen.innerHTML);
+    }  
 } 
 
+const bgContainer = document.querySelector(".container");
+bgContainer.onmouseover = () => bgContainer.setAttribute("class", "container mt-5 bg-dark p-5");
+bgContainer.onmouseout = () => bgContainer.setAttribute("class", "container mt-5 bg-warning p-5");
 
-
-
+// document.querySelector(".bg-secondary").onmouseover = function () {
+//     const bgContainer = document.querySelector(".bg-secondary");
+//     bgContainer.setAttribute("class", "bg-white p-2 rounded-3");
+//     // document.querySelector(".bg-secondary").style.backgroundColor = "white";
+//     console.log(bgContainer);
+// }
 
 
 
