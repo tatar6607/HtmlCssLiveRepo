da72d55c22f31d38c3e757eaf717b6b8f83a3dbb
Ozkan <tatar66@gmail.com>
2021-06-01 23:09:24 +0200

    added some fix

--- pages/Extras/yorumlar/assets/js/app.js
+++ pages/Extras/yorumlar/assets/js/app.js
@@ -1,3 +1,5 @@
+import yorumlar from "./data.js";
+
 // icerik locators
 const kisiResim = document.querySelector("#kişi-resim");
 const yazar = document.querySelector("#yazar");
@@ -8,56 +10,6 @@ const aciklamap = document.querySelector("#açıklama");
 const sol = document.querySelector(".geri-btn");
 const sag = document.querySelector(".ileri-btn");
 const sasirt = document.querySelector(".rasgele-div");
-
-// icerik
-let yorumlar = [
-  {
-    photo:
-      "https://cdn.pixabay.com/photo/2014/03/24/17/19/teacher-295387_960_720.png",
-    adSoyad: "Ayse Yilmaz",
-    meslek: "Web Tasarimci",
-    aciklama: "Bu urunler bir harika dostum.",
-  },
-
-  {
-    photo:
-      "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
-    adSoyad: "Ali Hamarta",
-    meslek: "Muhendis",
-    aciklama: "Ese dosta tavsiye ediyorum o derece...",
-  },
-
-  {
-    photo:
-      "https://cdn.pixabay.com/photo/2016/11/01/21/11/avatar-1789663_960_720.png",
-    adSoyad: "Ayhan Canli",
-    meslek: "Web Tasarimci",
-    aciklama: "Iyiki almisim fiyatlar uctu.",
-  },
-
-  {
-    photo:
-      "https://cdn.pixabay.com/photo/2021/05/25/06/55/child-6281555_960_720.png",
-    adSoyad: "Nukhet Duru",
-    meslek: "Sanatci",
-    aciklama: "Ikinci elde fiyat dusmuyor. Sagol GittiBurada :)",
-  },
-
-  {
-    photo:
-      "https://cdn.pixabay.com/photo/2016/12/13/16/17/dancer-1904467_960_720.png",
-    adSoyad: "Murtaza Elibol",
-    meslek: "Developer",
-    aciklama: "Kacirma derim tukenmeden al",
-  },
-
-  {
-    photo: "./assets/images/sample.png",
-    adSoyad: "Eric Jhonson",
-    meslek: "Gezgin",
-    aciklama: "Fiyat/Performans urunu beyler verdiginiz paraya degiyor :)",
-  },
-];
 
 let seciliYorum = 0;
 // console.log("toplam yorum", seciliYorum);
